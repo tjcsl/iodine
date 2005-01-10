68d42ad5fcc52b6f1b56dc89cca0087173ac0250
braujac <devnull@localhost>
2005-01-10 01:26:59 +0000

    Tailorized "2005-01-09 20:26:59 by braujac" Wrote most of the code to handle SELECT statements, in one possible implementation at least.

--- modules/mysql.mod.php
+++ modules/mysql.mod.php
@@ -9,6 +9,22 @@
 	*/
 	
 	class MySQL {
+
+		/**
+		* Indicates that results should be greater than a value.
+		*/
+		const $GREATER_THAN = '>';
+		/**
+		* Indicates that results should be less than a value.
+		*/
+		const $LESS_THAN = '<';
+		/**
+		* Indicates that results should be equal to a value.
+		*/
+		const $EQUAL_TO = '=';
+		
+		private var $db;
+		
 		/**
 		* The MySQL class constructor.
 		* 
@@ -16,36 +32,110 @@
 		*/
 		function MySQL() {
 			//TODO: Get config value here//
-			$this->connect($blah, $blah2, $blah3);
+			$db = $this->connect($blah, $blah2, $blah3);
 
 		}
+
+		/**
+		* Converts/wraps a MySQL result object into an Intranet2 type.
+		*
+		* @access private
+		* @param object $sql The MySQL resultset object.
+		* @return object An Intranet2-MySQL result object.
+		*/
+		private function sql_to_result($sql) {
+			//TODO:  Implement for real.  Decide on what results should be.
+			return $sql;
+		}
 		
-		function connect($server, $user, $password) {
-			mysql_pconnect($server, $user, $password);
+		protected function connect($server, $user, $password) {
+			return mysql_pconnect($server, $user, $password);
 		}
 
 		protected function select_db($database) {
 			mysql_select_db($database);
 		}
 
-		function query($query) {
-
+		protected function query($query) {
+			return mysql_query($db,$query); //Is this the right ordering?
 		}
 
-		function select($table, $columns = "", $where = "") {
-			query("SELECT .........");
+		/**
+		* Issues a proporly formatted MySQL SELECT query, and returns the results.
+		*
+		* @param string $table The table to query.
+		* @param array $columns The columns to select (all by default).
+		* @param assoc_array $where The conditions on which to accept a row.  This should
+		* be an associative array of two-element arrays consisting of a comparison operator
+		* and a value, so that $where[$key] = array($comparitive,$value).
+		* @param array $ordering The desired sort order of the resultset.
+		*/
+		function select($table, $columns = false, $where = false, $ordering = false) {
+			/*
+			** Build a (hopefully valid) MySQL query from the arguments.
+			*/
+			$q = "SELECT ";
+			if (!$columns) {
+				$q .= '*';
+			} else {
+				$first = true;
+				foreach ($columns as $col) {
+					if ($first) {
+						$first = false;
+					} else {
+						$q .= ',';
+					}
+					$q .= $col;
+				}
+			}
+			
+			$q .= "FROM $table";
+			
+			if ($where) {
+				$q .= " WHERE ";
+				$first = true;
+				foreach ($where as $key=>$subarray) {
+					if ($first) {
+						$first = false;
+					} else {
+						$q .= ',';
+					}
+					$comptype = $subarray[0];
+					$value = addslashes($subarray[1]); //Is addslashes() good enough?
+					$q .= "$key $comptype '$value'";
+				}
+			}
+			
+			if ($ordering) {
+				$q .= " ORDER BY ";
+				$first = true;
+				foreach ($ordering as $item) {
+					if ($first) {
+						$first = false;
+					} else {
+						$q .= ',';
+					}
+					$q .= $item;
+				}
+			}
+
+			//Glad that's over with.  Now, we query the database.
+			
+			return sql_to_result(query($q));
 		}
 
 		function insert($table, $columns, $values) {
 
 		}
 
-		function update($table, $columns, $values, $where = "") {
+		function update($table, $columns, $values, $where = false) {
 
 		}
 
-		function drop($table, $where = "0") {
+		function drop($table, $where = false) {
 			//If where is nonexistent, throw error
+
+			//braujac is confused: why is dropping everything not allowable?
 		}
 
 	}
