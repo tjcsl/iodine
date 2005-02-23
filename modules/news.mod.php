4aad0c5e99f6f596ba7dc69831a2bbebda857fb7
braujac <devnull@localhost>
2005-02-23 07:44:53 +0000

    Tailorized "2005-02-23 02:44:53 by braujac" Created initial framework as an example of what a module should look like. Hopefully this'll be the first module to actually DO something.

---
+++ modules/news.mod.php
@@ -0,0 +1,39 @@
+<?php
+
+	/**
+	* The module that keeps you hip and cool with the latest happenings
+	* the world (or at least the TJ Intranet) over!
+	*
+	* @author The Intranet 2 Development Team
+	*/
+
+	class News implements Module {
+		
+		private $display;
+		
+		/**
+		* The News class constructor.
+		*/
+		function __construct() {
+		}
+		
+		function init_pane() {
+		}
+		
+		function display_pane($display) {
+			$display->raw_display("This is today's news, in a pane. <BR />");
+		}
+		
+		function init_box() {
+		}
+
+		function display_box($display) {
+			$display->raw_display("This is today's news, in a box. <BR />");
+		}
+
+		function get_name() {
+			return "News";
+		}
+	}
+
+?>
