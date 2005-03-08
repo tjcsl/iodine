0ec34dd149880d3cb097babd93c70efc7ce45c46
braujac <devnull@localhost>
2005-03-08 06:47:52 +0000

    Tailorized "2005-03-08 01:47:52 by braujac" Fixed syntax errors and changed the story field to TEXT.

--- mysql/news.sql
+++ mysql/news.sql
@@ -1,11 +1,11 @@
-CREATE TABLE news (
+CREATE TABLE news_stories (
 	id bigint NOT NULL DEFAULT '0' AUTO_INCREMENT UNIQUE,	/*Unique autoid*/
 	PRIMARY KEY(id),				
-	text BIGTEXT NOT NULL,				/*Story text*/
+	text TEXT NOT NULL,				/*Story text*/
 	authorID bigint NOT NULL,			/*Student/teacher ID*/
 	authortype ENUM('student','teacher','other') DEFAULT 'student',	/*Poster type*/
 	title VARCHAR(255) NOT NULL,			/*Story brief title*/
-	posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,	/*Date posted*/
+	posted TIMESTAMP NOT NULL DEFAULT 'CURRENT_TIMESTAMP',	/*Date posted*/
 	KEY(posted),					
-	revised TIMESTAMP ON UPDATE CURRENT_TIMESTAMP	/*Date revised*/
+	revised TIMESTAMP DEFAULT 'CURRENT_TIMESTAMP'	/*Date revised*/
 );
