335ed5bb80b60b41485f019f994c14d938cc5771
adeason <devnull@localhost>
2005-07-07 10:08:53 +0000

    Tailorized "2005-07-07 06:08:53 by adeason" Added the 'author' column for the author's name, and changed around the posted and revised columns. So, now when you add a row, you must explicitly set 'posted' to either NULL or the current time. And whenever the row is updated, the 'revised' column will automatically update to the current time.

--- mysql/news.sql
+++ mysql/news.sql
@@ -2,10 +2,11 @@ CREATE TABLE news_stories (
 	id bigint NOT NULL DEFAULT '0' AUTO_INCREMENT UNIQUE,	/*Unique autoid*/
 	PRIMARY KEY(id),				
 	text TEXT NOT NULL,				/*Story text*/
-	authorID bigint NOT NULL,			/*Student/teacher ID*/
+	author varchar(128) NOT NULL,		/* author name */
+	authorID bigint,			/*Student/teacher ID*/
 	authortype ENUM('student','teacher','other') DEFAULT 'student',	/*Poster type*/
 	title VARCHAR(255) NOT NULL,			/*Story brief title*/
+	revised TIMESTAMP NOT NULL DEFAULT 'CURRENT_TIMESTAMP',	/*Date revised*/
 	posted TIMESTAMP NOT NULL DEFAULT 'CURRENT_TIMESTAMP',	/*Date posted*/
-	KEY(posted),					
-	revised TIMESTAMP DEFAULT 'CURRENT_TIMESTAMP'	/*Date revised*/
+	KEY(posted)
 );
