67e6b423372f7bee455535c257109b3d09918e0c
adeason <devnull@localhost>
2005-07-10 07:22:37 +0000

    Tailorized "2005-07-10 03:22:37 by adeason" Random changes. Don't ask me why, I'm just weird like that. (Look at diffs for actual changes, not much changed.)

--- mysql/news.sql
+++ mysql/news.sql
@@ -1,11 +1,11 @@
 CREATE TABLE news(
-	id bigint NOT NULL DEFAULT '0' AUTO_INCREMENT UNIQUE,	/*Unique autoid*/
+	id INT UNSIGNED NOT NULL DEFAULT '0' AUTO_INCREMENT UNIQUE, /*Unique autoid*/
 	PRIMARY KEY(id),				
-	text TEXT NOT NULL,				/*Story text*/
+	title VARCHAR(256) NOT NULL,			/*Story title*/
+	text TEXT NOT NULL,			/*Story text*/
 	author varchar(128) NOT NULL,		/* author name */
-	authorID bigint,			/*Student/teacher ID*/
+	authorID MEDIUMINT UNSIGNED,			/*Student/teacher ID*/
 	authortype ENUM('student','teacher','other') DEFAULT 'student',	/*Poster type*/
-	title VARCHAR(255) NOT NULL,			/*Story brief title*/
 	revised TIMESTAMP NOT NULL DEFAULT 'CURRENT_TIMESTAMP',	/*Date revised*/
 	posted TIMESTAMP NOT NULL DEFAULT 'CURRENT_TIMESTAMP',	/*Date posted*/
 	KEY(posted)
