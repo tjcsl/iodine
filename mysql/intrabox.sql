7989f7c2e5279ae55d87c4e02b715678dd98f20e
adeason <devnull@localhost>
2005-07-30 08:42:52 +0000

    Tailorized "2005-07-30 04:42:52 by adeason" Added the display_name column, so intraboxes can have a fancier name when referring to them in various user interfaces (such as selecting ones to delete/add).

--- mysql/intrabox.sql
+++ mysql/intrabox.sql
@@ -2,6 +2,8 @@ CREATE TABLE intrabox (
 	boxid INT UNSIGNED NOT NULL DEFAULT '0' AUTO_INCREMENT UNIQUE, /*Unique autoid*/
 	PRIMARY KEY(boxid),	
 	
-	name VARCHAR(63) NOT NULL
+	name VARCHAR(63) NOT NULL,
+
+	display_name VARCHAR(127)
 	/* Put some ACL/permission stuff down here */
 );
