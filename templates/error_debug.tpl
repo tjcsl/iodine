9e0ef58490d38014249d1cf783443b3a60128d94
sgross <sgross@humboldt.tjhsst.edu>
2005-10-07 19:06:14 -0400

    Resizable error/debug boxes!

--- templates/error_debug.tpl
+++ templates/error_debug.tpl
@@ -1,30 +1,76 @@
-<script language="JavaScript">
-function minimize(name) {
-	var div = document.getElementById(name);
-	if (/ minimized/.test(div.className)) {
-		div.style.width = [<if $debug && $errors>]style="45%"[<else>]style="90%"[</if>];
-		div.style.height = "";
-		div.className = div.className.replace(/ minimized/, "");
-	} else {
-		div.style.width = "";
-		div.style.height = "";
-		div.className += " minimized";
-	}
+<script language="javascript" src="[<$I2_ROOT>]www/js/cookie.js"></script>
+<script language="javascript" src="[<$I2_ROOT>]www/js/genresize.js"></script>
+<script language="javascript" src="[<$I2_ROOT>]www/js/ieemu.js"></script>
+<script language="javascript">
+if (moz) {
+	extendElementModel();
+	extendEventObject();
+	emulateEventHandlers(["mousemove", "mousedown", "mouseup"]);
 }
 </script>
+<script language="JavaScript">
+var divs = Array();
+divs['error'] = 
+{ 
+	width : getCookie('error_width', '[<if $debug && $errors>]45%[<else>]90%[</if>]'),
+	height : getCookie('error_height', '')
+};
+
+divs['debug'] = 
+{
+	width : getCookie('debug_width', '[<if $debug && $errors>]45%[<else>]90%[</if>]'),
+	height : getCookie('debug_height', '')
+};
+
+function swap(id) {
+	var div = document.getElementById(id);
+	if (/ minimized/.test(div.className)) {
+		maximize(div);
+		setCookie(id + "_open", "true");
+	} else {
+		minimize(div);
+		setCookie(id + "_open", "false");
+	}
+}
+
+function minimize(div) {
+	divs[div.id].width = div.style.width;
+	divs[div.id].height = div.style.height;
+	div.style.width = "";
+	div.style.height = "";
+	div.className = div.className.replace(/ resizeMe/, "");
+	div.className += " minimized";
+}
+
+function maximize(div) {
+	div.style.width = divs[div.id].width;
+	div.style.height = divs[div.id].height;
+	div.className = div.className.replace(/ minimized/, "");
+	div.className += " resizeMe";
+}
+
+</script>
 [<if $errors>]
- <div class="error" [<if $debug>]style="width:45%;"[<else>]style="width:90%;"[</if>] id="error">
-  <div class="button_container"><div class="minimize" onclick="minimize('error')"></div></div>
-  Iodine has encountered the following errors: <br /><br />
+ <div class="error minimized" id="error">
+  <div class="button_container"><div class="minimize" onclick="swap('error')"></div></div>
+  Iodine has encountered the following errors:
   [<$errors>]
  </div>
 [</if>]
 [<if $debug>]
  <div class="debug minimized" id="debug">
-  <div class="button_container"><div class="minimize" onclick="minimize('debug')"></div></div>
-  Debug messages: <br /><br />
+  <div class="button_container"><div class="minimize" onclick="swap('debug')"></div></div>
+  Debug messages:
   [<$debug>]
  </div>
 [</if>]
+<script language="javascript">
+if (eval(getCookie('error_open', 'true'))) {
+	maximize(document.getElementById('error'));
+}
+if (eval(getCookie('debug_open', 'false'))) {
+	maximize(document.getElementById('debug'));
+}
+</script>
 </body>
 </html>
---
+++ www/js/genresize.js
@@ -0,0 +1,135 @@
+/////////////////////////////////////////////////////////////////////////
+// Generic Resize by Erik Arvidsson                                    //
+//                                                                     //
+// You may use this script as long as this disclaimer is remained.     //
+// See www.dtek.chalmers.se/~d96erik/dhtml/ for mor info               //
+//                                                                     //
+// How to use this script!                                             //
+// Link the script in the HEAD and create a container (DIV, preferable //
+// absolute positioned) and add the class="resizeMe" to it.            //
+/////////////////////////////////////////////////////////////////////////
+
+
+var regex = /resizeMe/;
+
+var theobject = null; //This gets a value as soon as a resize start
+
+function resizeObject() {
+	this.el        = null; //pointer to the object
+	this.dir    = "";      //type of current resize (n, s, e, w, ne, nw, se, sw)
+	this.grabx = null;     //Some useful values
+	this.graby = null;
+	this.width = null;
+	this.height = null;
+	this.left = null;
+	this.top = null;
+}
+	
+
+//Find out what kind of resize! Return a string inlcluding the directions
+function getDirection(el) {
+	var xPos, yPos, offset, dir;
+	dir = "";
+
+	xPos = window.event.offsetX;
+	yPos = window.event.offsetY;
+	window.status = xPos + ", " + yPos;
+
+	offset = 7; //The distance from the edge in pixels
+
+	if (yPos<offset) dir += "n";
+	else if (yPos > el.offsetHeight-offset) dir += "s";
+	if (xPos<offset) dir += "w";
+	else if (xPos > el.offsetWidth-offset) dir += "e";
+
+	return dir;
+}
+
+function doDown() {
+	var el = getReal(event.srcElement, "className", regex);
+
+	if (el == null || !regex.test(el.className)) {
+		theobject = null;
+		return;
+	}		
+
+	dir = getDirection(el);
+	if (dir == "") return;
+
+	theobject = new resizeObject();
+		
+	theobject.el = el;
+	theobject.dir = dir;
+
+	theobject.grabx = window.event.clientX;
+	theobject.graby = window.event.clientY;
+	theobject.width = el.clientWidth - 10;
+	theobject.height = el.clientHeight - 10;
+	theobject.left = el.offsetLeft;
+	theobject.top = el.offsetTop;
+
+	window.event.returnValue = false;
+	window.event.cancelBubble = true;
+}
+
+function doUp() {
+	if (theobject != null) {
+		theobject = null;
+	}
+}
+
+function doMove() {
+	var el, xPos, yPos, str, xMin, yMin;
+	xMin = 8; //The smallest width possible
+	yMin = 8; //             height
+	
+	el = getReal(event.srcElement, "className", regex);
+	if (/resizeMe/g.test(el.className)) {
+		str = getDirection(el);
+		//Fix the cursor	
+		if (str == "") str = "default";
+		else str += "-resize";
+		el.style.cursor = str;
+	}
+
+	//Dragging starts here
+	if(theobject != null) {
+		if (dir.indexOf("e") != -1)
+			theobject.el.style.width = Math.max(xMin, theobject.width + window.event.clientX - theobject.grabx) + "px";
+	
+		if (dir.indexOf("s") != -1)
+			theobject.el.style.height = Math.max(yMin, theobject.height + window.event.clientY - theobject.graby) + "px";
+
+		if (dir.indexOf("w") != -1) {
+			//theobject.el.style.left = Math.min(theobject.left + window.event.clientX - theobject.grabx, theobject.left + theobject.width - xMin) + "px";
+			theobject.el.style.width = Math.max(xMin, theobject.width - window.event.clientX + theobject.grabx) + "px";
+		}
+		if (dir.indexOf("n") != -1) {
+			//theobject.el.style.top = Math.min(theobject.top + window.event.clientY - theobject.graby, theobject.top + theobject.height - yMin) + "px";
+			theobject.el.style.height = Math.max(yMin, theobject.height - window.event.clientY + theobject.graby) + "px";
+		}
+		
+		window.event.returnValue = false;
+		window.event.cancelBubble = true;
+	
+		setCookie(theobject.el.id + "_width", theobject.el.style.width, '/');
+		setCookie(theobject.el.id + "_height", theobject.el.style.height, '/');
+	}
+}
+
+
+function getReal(el, type, testExp) {
+	temp = el;
+	/*while ((temp != null) && (temp.tagName != "BODY")) {
+		if (testExp.test(eval("temp." + type))) {
+			el = temp;
+			return el;
+		}
+		temp = temp.parentElement;
+	}*/
+	return el;
+}
+
+document.onmousedown = doDown;
+document.onmouseup   = doUp;
+document.onmousemove = doMove;
