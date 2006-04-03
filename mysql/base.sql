54f72e85a88b4cdcadc2d566d6035c63c10afb4c
braujac <devnull@localhost>
2006-04-02 22:52:32 -0400

    Changes finished, data imported.  I've done the heavy lifting for you.

---
+++ mysql/base.sql
@@ -0,0 +1,7 @@
+INSERT INTO groups (name,description) VALUES('admin_all','The grand admin group');
+INSERT INTO groups (name,description) VALUES('admin_ldap','The LDAP admin group');
+INSERT INTO groups (name,description) VALUES('admin_mysql','The MySQL admin group');
+INSERT INTO groups (name,description) VALUES('admin_eighth','The eighth admin group');
+INSERT INTO groups (name,description) VALUES('admin_groups','The groups admin group');
+INSERT INTO groups (name,description) VALUES('admin_news','The news admin group');
+INSERT INTO intrabox (name,display_name) VALUES('birthdays','birthdays');
