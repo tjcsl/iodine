b79a6f056410da0d6e07d0fe582b94bb0fcdf8c6
mbkremer <devnull@localhost>
2008-04-24 12:46:38 -0400

    Unused tables, empty in database

--- mysql/student_section_map.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS student_section_map;
-CREATE TABLE student_section_map (
-	studentid MEDIUMINT UNSIGNED NOT NULL,
-	KEY(studentid),
-	sectionid INT UNSIGNED NOT NULL,
-	KEY(sectionid)
-);
