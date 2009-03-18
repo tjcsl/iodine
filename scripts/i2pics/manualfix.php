d2216a1fc2716720c0cf46a9d71d421a83249e4c
wyang <devnull@localhost>
2009-03-18 08:41:26 -0400

    Add an old PHP script for one-offs; use similarly with the other PHP picture import script.

---
+++ scripts/i2pics/manualfix.php
@@ -0,0 +1,25 @@
+<?php
+/***
+ */
+$managerpswd = 	"MANAGERPSWDHERE"
+
+$search =	"username";
+$class =	"senior";
+$path =		"/home/200809pics_seniorformal/lifetouch/images"
+$file =		"noid00000.jpg";
+/***
+ */
+$ldapconn = ldap_connect('iodine-ldap.tjhsst.edu');
+ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+$bind = ldap_bind($ldapconn,"cn=Manager,dc=tjhsst,dc=edu",$managerpswd);
+$udn = ldap_search($ldapconn, 'ou=people,dc=tjhsst,dc=edu', "iodineuid={$search}", array('dn'));
+$info = ldap_get_entries($ldapconn, $udn);
+if($info['count']==0)
+	continue;
+echo "dn: cn={$class}Photo,";
+echo $info[0]["dn"] . "\r\n";
+echo "cn: {$class}Photo\r\n";
+echo "objectClass: iodinePhoto\r\njpegPhoto::";
+echo base64_encode(`convert "{$path}"/"{$file}" -format "jpeg" -resize 172x228 -`);
+echo "\r\n\r\n";
+?>
