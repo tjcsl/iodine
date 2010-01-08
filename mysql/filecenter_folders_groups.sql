810140b02d1b47812912a33ae88e314a26d91102
2011dmorris <devnull@localhost>
2010-01-07 21:14:34 -0500

    Change the mysql file that generates the filecenter links so that the default links are added upon table creation.

--- mysql/filecenter_folders_groups.sql
+++ mysql/filecenter_folders_groups.sql
@@ -4,3 +4,13 @@ CREATE TABLE filecenter_folders_groups (
 	path VARCHAR(255) NOT NULL,
 	name VARCHAR(255) NOT NULL
 );
+
+
+INSERT INTO filecenter_folders_groups VALUES (13,"https://shares.tjhsst.edu/{{tj01path}}","View your Windows (M:) files");
+INSERT INTO filecenter_folders_groups VALUES (13,"https://shares.tjhsst.edu/portfolio/{{grad_year}}/{{i2_username}}","View your portfolio");
+INSERT INTO filecenter_folders_groups VALUES (13,"https://shares.tjhsst.edu/upload2.php","Upload to Windows files");
+INSERT INTO filecenter_folders_groups VALUES (1,"https://shares.tjhsst.edu/","View all Windows files");
+INSERT INTO filecenter_folders_groups VALUES (1,"{{I2_ROOT}}filecenter/main/{{studentorstaff}}/{{i2_username}}/","Access your UNIX files");
+INSERT INTO filecenter_folders_groups VALUES (1,"{{I2_ROOT}}filecenter/csl/user/{{csl_username}}/","Access your old Systems Lab files");
+INSERT INTO filecenter_folders_groups VALUES (8,"https://shares.tjhsst.edu/Staff","Staff Intranet");
+INSERT INTO filecenter_folders_groups VALUES (2,"{{I2_ROOT}}filecenter/main/","AFS Root");
