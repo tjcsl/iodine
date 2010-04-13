d2ca18ef6f0e6b8eb565a913d652090cf52fb4ac
2011dmorris <devnull@localhost>
2010-04-12 20:40:56 -0400

    Make the ajax command line for cliodine actually work.

--- www/js/cliodine.js
+++ www/js/cliodine.js
@@ -3,6 +3,35 @@ var ltext;
 var rtext;
 var cursor;
 var inputspan;
+var idoffset=0;
+
+var ajaxRequest;
+
+try {
+	ajaxRequest= new XMLHttpRequest();
+} catch (e) {
+	try {
+		ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+	} catch (e) {
+		try {
+			ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+		} catch(e) {
+			alert("Sorry, won't work. Going back to normal iodine");
+			window.location="../";
+		}
+	}
+}
+
+ajaxRequest.onreadystatechange = function(){
+	if(ajaxRequest.readyState == 4){
+		var i = document.createElement("div");
+		i.innerHTML=ajaxRequest.responseText;
+		document.getElementById("terminal").appendChild(i);
+		idoffset+=1;
+		newprompt();
+	}
+}
+
 function pressed(e) {
 	if( e.which>=32 && e.which<=126) {
 		var character=String.fromCharCode(e.which)
@@ -11,10 +40,15 @@ function pressed(e) {
 	if(letter) {
 		//alert(letter+"_:_"+character+"__"+ctrlpressed);
 		ltext.innerHTML=ltext.innerHTML+character;
-	} else if (e.which==8) {
+	} else if (e.which==8) { // Backspace
 		if(ltext.innerHTML.length >0) {
 			ltext.innerHTML=ltext.innerHTML.substring(0,ltext.innerHTML.length-1);
 		}
+	} else if (e.which==13) { // Enter
+		var str=ltext.innerHTML+cursor.innerHTML+rtext.innerHTML;
+		str=str.replace(" ","/");
+		ajaxRequest.open("GET",str,true);
+		ajaxRequest.send();
 	}
 }
 function specialdown(e) {
@@ -27,22 +61,23 @@ function specialup(e) {
 }
 function newprompt() {
 	inputspan = document.createElement("span");
-	 inputspan.id="inputspan";
+	 inputspan.id="inputspan"+idoffset;
 	 var promptstatic = document.createElement("span");
-	  promptstatic.id="promptstatic";
+	  promptstatic.id="promptstatic"+idoffset;
 	  var j = document.createTextNode(username + "@iodine:~$ ");
 	  promptstatic.appendChild(j);
 	 inputspan.appendChild(promptstatic);
 	 ltext = document.createElement("span");
-	  ltext.id="ltext";
+	  ltext.id="ltext"+idoffset;
 	 inputspan.appendChild(ltext);
 	 cursor= document.createElement("span");
-	  cursor.id="cursor";
+	  cursor.id="cursor"+idoffset;
 	 inputspan.appendChild(cursor);
 	 rtext = document.createElement("span");
-	  rtext.id="rtext";
+	  rtext.id="rtext"+idoffset;
 	 inputspan.appendChild(rtext);
 	document.getElementById("terminal").appendChild(inputspan);
+	document.getElementById("terminal").appendChild(document.createElement("br"));
 }
 function init() {
 	document.onkeypress=function(e){ pressed(e)};
