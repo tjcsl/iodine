f27c417f213c8abcf4a71c584b690c09439cf874
2011dmorris <devnull@localhost>
2010-04-12 21:19:02 -0400

    Make cliodine look much better and make it work more.

--- www/js/cliodine.js
+++ www/js/cliodine.js
@@ -4,6 +4,7 @@ var rtext;
 var cursor;
 var inputspan;
 var idoffset=0;
+var timer;
 
 var ajaxRequest;
 
@@ -27,6 +28,7 @@ ajaxRequest.onreadystatechange = function(){
 		var i = document.createElement("div");
 		i.innerHTML=ajaxRequest.responseText;
 		document.getElementById("terminal").appendChild(i);
+		cursor.setAttribute("class","darkcursor");
 		idoffset+=1;
 		newprompt();
 	}
@@ -40,12 +42,17 @@ function pressed(e) {
 	if(letter) {
 		//alert(letter+"_:_"+character+"__"+ctrlpressed);
 		ltext.innerHTML=ltext.innerHTML+character;
+		clearTimeout(timer);
+		brighten();
 	} else if (e.which==8) { // Backspace
 		if(ltext.innerHTML.length >0) {
 			ltext.innerHTML=ltext.innerHTML.substring(0,ltext.innerHTML.length-1);
 		}
+		clearTimeout(timer);
+		brighten();
 	} else if (e.which==13) { // Enter
 		var str=ltext.innerHTML+cursor.innerHTML+rtext.innerHTML;
+		str=str.replace("&nbsp;"," ");
 		str=str.replace(" ","/");
 		ajaxRequest.open("GET",str,true);
 		ajaxRequest.send();
@@ -62,6 +69,7 @@ function specialup(e) {
 function newprompt() {
 	inputspan = document.createElement("span");
 	 inputspan.id="inputspan"+idoffset;
+	 inputspan.setAttribute("class","inputspan");
 	 var promptstatic = document.createElement("span");
 	  promptstatic.id="promptstatic"+idoffset;
 	  var j = document.createTextNode(username + "@iodine:~$ ");
@@ -69,19 +77,33 @@ function newprompt() {
 	 inputspan.appendChild(promptstatic);
 	 ltext = document.createElement("span");
 	  ltext.id="ltext"+idoffset;
+	  ltext.setAttribute("class","ltext");
 	 inputspan.appendChild(ltext);
 	 cursor= document.createElement("span");
 	  cursor.id="cursor"+idoffset;
+	  cursor.setAttribute("class","cursor");
+	  cursor.setAttribute("width","8px");
+	  cursor.innerHTML="&nbsp";
 	 inputspan.appendChild(cursor);
 	 rtext = document.createElement("span");
 	  rtext.id="rtext"+idoffset;
+	  rtext.setAttribute("class","rtext");
 	 inputspan.appendChild(rtext);
 	document.getElementById("terminal").appendChild(inputspan);
 	document.getElementById("terminal").appendChild(document.createElement("br"));
+}
+function brighten() {
+	cursor.setAttribute("class","cursor");
+	timer=setTimeout("darken()",500);
+}
+function darken() {
+	cursor.setAttribute("class","darkcursor");
+	timer=setTimeout("brighten()",500);
 }
 function init() {
 	document.onkeypress=function(e){ pressed(e)};
 	document.onkeydown= function(e){ specialdown(e)};
 	document.onkeyup=   function(e){ specialup(e)};
 	newprompt();
+	timer=setTimeout("darken()",500);
 }
