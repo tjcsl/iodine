fb41ecd5dde7319ef64604e89733e85c0ec18679
2011dmorris <devnull@localhost>
2010-07-09 12:14:54 -0400

    Get the groups in news editing to show up in all browsers correctly. (needs test with IE5-, IE6, and IE7 though)

--- www/js/news_groups.js
+++ www/js/news_groups.js
@@ -7,15 +7,45 @@ function news_addGroup(gid) {
 	var select = document.createElement("SELECT");
 	select.name="add_groups[]";
 	select.className="groups_list";
-	for (var i = 1; i < groupsList.options.length; i+=1) {
-		var option = groupsList.options[i];
-		var newOption = document.createElement("OPTION");
-		newOption.value = option.value;
-		newOption.text=option.text;
-		if (gid == newOption.value) {
-			newOption.selected = true;
+	//Different method for ie5- and ie6+. Both work in everything else.
+	var method=0;
+	if(navigator.appName == 'Microsoft Internet Explorer') {
+		if (navigator.appName == 'Microsoft Internet Explorer')
+		{
+			var ua = navigator.userAgent;
+			var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+			if (re.exec(ua) != null)
+				if(parseFloat( RegExp.$1 )>5){
+					method=1;
+				}
 		}
-		select.appendChild(newOption);
+	}
+	if(method==0) {//Everyone else + IE5-
+		for (var i = 1; i < groupsList.options.length; i+=1) {
+			var option = groupsList.options[i];
+			var newOption = document.createElement("OPTION");
+			newOption.value = option.value;
+			/*newOptiontext=document.createTextNode(option.text);
+			newOption.appendChild(newOptiontext);*/
+			newOption.text=option.text;
+			if (gid == newOption.value) {
+				newOption.selected = true;
+			}
+			select.appendChild(newOption);
+		}
+	} else {//IE6+
+		for (var i = 1; i < groupsList.options.length; i+=1) {
+			var option = groupsList.options[i];
+			var newOption = document.createElement("OPTION");
+			newOption.value = option.value;
+			newOptiontext=document.createTextNode(option.text);
+			newOption.appendChild(newOptiontext);
+			//newOption.text=option.text;
+			if (gid == newOption.value) {
+				newOption.selected = true;
+			}
+			select.appendChild(newOption);
+		}
 	}
 	row.insertCell(1).appendChild(select);
 	
