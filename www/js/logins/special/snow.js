3ea3a5debaf9d74d1e9426cddfef667b2accf17e
2011dmorris <devnull@localhost>
2010-12-03 09:05:52 -0500

    Add a function to the snow.js login page, so that now the argument parser can handle unicode.

--- www/js/logins/special/snow.js
+++ www/js/logins/special/snow.js
@@ -68,7 +68,39 @@ function set_flakes() {
 	var regex = new RegExp("[\\?&]flake=([^&#]*)");
 	var results = regex.exec( window.location.href );
 	if( results != null )
-		snowletter=results[1];
+		snowletter=url_decode(results[1]);
+}
+
+function url_decode(utftext) {
+	//
+	// Credit for the base for this function
+	// goes to the people at webtoolkit
+	// http://www.webtoolkit.info/
+	// Pretty cool code, imho.
+	//
+	utftext=unescape(utftext);
+	var string = "";
+	var i = 0;
+	var c = c1 = c2 = 0;
+	
+	while ( i < utftext.length ) {
+		c = utftext.charCodeAt(i);
+		if (c < 128) {
+			string += String.fromCharCode(c);
+			i++;
+		} else if((c > 191) && (c < 224)) {
+			c2 = utftext.charCodeAt(i+1);
+			string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+			i += 2;
+		} else {
+			c2 = utftext.charCodeAt(i+1);
+			c3 = utftext.charCodeAt(i+2);
+			string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+			i += 3;
+		}
+	}
+	string = string.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
+	return string;
 }
 
 window.onresize=resize;
