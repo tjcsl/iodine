ad722a7314cb77af7e4f942729256b0f1b87a123
2012zyaro <devnull@localhost>
2010-12-14 19:18:11 -0500

    - Added modified header ("Welcome to the grid" instead of just "Welcome")

--- javascriptstyles/tron.js
+++ javascriptstyles/tron.js
@@ -1,2 +1,9 @@
 function page_init() {
+	try { // because old browsers may not support getElementsByClassName
+		var welcomeTxt = document.getElementsByClassName("title")[0].innerHTML;
+		welcomeTxt = (welcomeTxt.substring(0, welcomeTxt.indexOf(",")) + " to the grid" + welcomeTxt.substring(welcomeTxt.indexOf(",")));
+		document.getElementsByClassName("title")[0].innerHTML = welcomeTxt;
+	} catch(e) {
+//		alert("Your browser fails.")
+	}
 }
