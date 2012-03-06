28cde2f3d3f398dd903a8e6e49177c2d21852f1b
Zachary Yaro <zmyaro@gmail.com>
2012-03-06 12:22:19 -0500

    Added "i3-dark" theme

---
+++ javascriptstyles/i3-dark.js
@@ -0,0 +1,26 @@
+preload();
+function preload() {
+}
+function page_init() {
+	//shrinkBlurb();
+}
+function intrabox_onmouseover(div_id) {
+}
+function intrabox_onmouseout(div_id) {
+}
+function menu_onmouseover() {
+}
+function menu_onmouseout() {
+}
+function menuitem_onmouseover(div_id,not_1,not_2,not_3,not_4,not_5) {
+}
+function menuitem_onmouseout(div_id) {i
+}
+/*function shrinkBlurb() { // remove the date from the header blurb
+	if(window.innerWidth < 575) {
+		var blurb = document.getElementsByClassName("blurb")[0];
+		blurb.innerHTML = blurb.innerHTML.replace(/Today is .*?\. /, "");
+		window.removeEventListener("resize", shrinkBlurb);
+	}
+}
+window.addEventListener("resize", shrinkBlurb, false);*/
