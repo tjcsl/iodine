6e883575330263c7202a948079566a9813da2a87
Peter Foley <pefoley2@verizon.net>
2013-05-18 09:34:35 -0400

    refactor mysql db init files

--- mysql/base/groups_name.sql
+++ mysql/base/groups_name.sql
@@ -16,20 +16,3 @@ INSERT INTO groups_name (name,description) VALUES('admin_eighth_signup','Eighth 
 INSERT INTO groups_name (name,description) VALUES('admin_testing','Testing administrators');
 INSERT INTO groups_name (name,description) VALUES('admin_aphorisms','Aphorism administrators');
 INSERT INTO groups_name (name,description) VALUES('admin_calendar','Calendar administrators');
-/* intraboxes */
-INSERT INTO intrabox (name,display_name) VALUES('news','News');
-INSERT INTO intrabox (name,display_name) VALUES('eighth','Eighth Period');
-INSERT INTO intrabox (name,display_name) VALUES('mail','Your Mail');
-INSERT INTO intrabox (name,display_name) VALUES('filecenter','Your Files');
-INSERT INTO intrabox (name,display_name) VALUES('birthdays','Birthdays');
-INSERT INTO intrabox (name,display_name) VALUES('studentdirectory','Student Directory');
-INSERT INTO intrabox (name,display_name) VALUES('links','Useful Links');
-INSERT INTO intrabox (name,display_name) VALUES('scratchpad','ScratchPad');
-INSERT INTO intrabox (name,display_name) VALUES('seniors','Senior College Destinations');
-INSERT INTO intrabox (name,display_name) VALUES('weather','TJ Weather Info');
-INSERT INTO intrabox (name,display_name) VALUES('devlinks','Developer Links');
-INSERT INTO intrabox (name,display_name) VALUES('polls','Polls');
-INSERT INTO intrabox (name,display_name) VALUES('countdown','Graduation countdown');
-INSERT INTO intrabox (name,display_name) VALUES('testing','Testing Information');
-INSERT INTO intrabox (name,display_name) VALUES('majora','Majora\'s Mask Countdown');
-INSERT INTO intrabox (name,display_name) VALUES('bellschedule','Bell Schedule');
--- mysql/filecenter_folders_groups.sql
+++ mysql/filecenter_folders_groups.sql
@@ -4,16 +4,3 @@ CREATE TABLE filecenter_folders_groups (
 	path VARCHAR(255) NOT NULL,
 	name VARCHAR(255) NOT NULL
 );
-
-
-INSERT INTO filecenter_folders_groups VALUES (13,"{{I2_ROOT}}filecenter/common/","View/Upload your Common (R:) files");
-INSERT INTO filecenter_folders_groups VALUES (13,"{{I2_ROOT}}filecenter/local/","View/Upload your Windows (M:) files");
-INSERT INTO filecenter_folders_groups VALUES (13,"{{I2_ROOT}}filecenter/portfolio/{{grad_year}}/{{i2_username}}","View your portfolio");
-INSERT INTO filecenter_folders_groups VALUES (13,"https://shares.tjhsst.edu/portfolio/{{grad_year}}/{{i2_username}}","View your portfolio (Shares)");
-INSERT INTO filecenter_folders_groups VALUES (1,"https://shares.tjhsst.edu/","View all Windows files (Shares)");
-INSERT INTO filecenter_folders_groups VALUES (1,"{{I2_ROOT}}filecenter/main/{{studentorstaff}}/{{i2_username}}/","Access your UNIX files");
-INSERT INTO filecenter_folders_groups VALUES (1,"{{I2_ROOT}}filecenter/csl/user/{{csl_username}}/","Access your old Systems Lab files");
-INSERT INTO filecenter_folders_groups VALUES (8,"https://shares.tjhsst.edu/Staff","Staff Intranet");
-INSERT INTO filecenter_folders_groups VALUES (2,"{{I2_ROOT}}filecenter/main/","AFS Root");
-INSERT INTO filecenter_folders_groups VALUES (13,"https://shares.tjhsst.edu/{{tj01path}}","View your Windows (M:) files (Shares)");
-INSERT INTO filecenter_folders_groups VALUES (13,"https://shares.tjhsst.edu/upload2.php","Upload to Windows files (Shares)");
