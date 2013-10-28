18d3d633e52228b9bb21322978693dc158dd16b1
James Woglom <j@wogloms.net>
2013-10-27 22:53:06 -0400

    add dayschedule mysql table info

---
+++ mysql/dayschedule.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `dayschedule_custom_summaries` (
+  `daytype` varchar(32) NOT NULL,
+  `daydesc` varchar(128) NOT NULL
+)
+CREATE TABLE IF NOT EXISTS `dayschedule_pretty_summaries` (
+  `daytype` varchar(32) NOT NULL,
+  `daydesc` varchar(128) NOT NULL
+)
+CREATE TABLE IF NOT EXISTS `dayschedule_custom_schedules` (
+  `daytype` varchar(32) NOT NULL,
+  `json` varchar(2048) NOT NULL
+)
