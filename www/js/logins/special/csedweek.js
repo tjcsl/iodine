4373b3f07794306407a2d9a293ecda273673f04f
James Woglom <j@wogloms.net>
2013-12-06 19:17:36 -0500

    update csedweek javascript, make it randomly select source files to display

--- www/js/logins/special/csedweek.js
+++ www/js/logins/special/csedweek.js
@@ -1,8 +1,10 @@
 //Configuration Options
 var interval = 100;
-var stepsize = 6;
-var codefile = i2root+"/www/js/logins/special/csedweek.txt";
-
+var stepsize = 12;
+//var codefile = i2root+"/www/js/logins/special/csedweek.txt";
+var git = "/git/?p=intranet2.git;a=blob_plain;f=modules/";
+var files = [git+"eighth/eighth.mod.php5",git+"news/news.mod.php5",git+"dayschedule/dayschedule.mod.php5",git+"studentdirectory/studentdirectory.mod.php5",git+"news/news.mod.php5",i2root+"www/js/logins/special/csedweek-ion.txt"];
+var codefile = files[Math.floor((+new Date/100)%files.length)];
 //Globals
 var codediv;
 var code = "";
@@ -19,8 +21,7 @@ try {
 		try {
 			ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
 		} catch(e) {
-			alert("Sorry, won't work. Going back to normal iodine");
-			window.location="../";
+			console.log("Sorry, won't work. Going back to normal iodine");
 		}
 	}
 }
@@ -28,7 +29,7 @@ ajaxRequest.onreadystatechange = function(){
 	if(ajaxRequest.readyState == 4){
 		code = ajaxRequest.responseText;
 		index=0;
-		window.setInterval(step,100);
+		codeint = window.setInterval(step,100);
 	}
 }
 
@@ -36,7 +37,7 @@ ajaxRequest.onreadystatechange = function(){
 function initcode() {
 	codediv = document.createElement("div");
 	codediv.id="codediv";
-	codediv.innerHTML="";
+	codediv.innerHTML="<a href='http://csedweek.org' style='color:inherit;text-decoration:none'><div style='position:fixed;top:0;width:100%;z-index:1;font-family:inherit;white-space:inherit;background:black'>/*\n * Happy Computer Science Education Week from the Sysadmins!\n */\n</div></a>";
 	codediv.style.position="absolute";
 	codediv.style.top="0px";
 	codediv.style.left="570px";
@@ -46,6 +47,7 @@ function initcode() {
 	codediv.style.fontFamily="monospace";
 	codediv.style.overflow="hidden";
 	codediv.style.whiteSpace="pre";
+	codediv.style.paddingTop="60px";
 	document.getElementsByTagName("body")[0].appendChild(codediv);
 	ajaxRequest.open("GET",codefile,true);
 	ajaxRequest.send(null);
