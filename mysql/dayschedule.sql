8c1f8306db06b3f30ae0de36b06c9206259af01e
Peter Foley <pefoley2@verizon.net>
2014-09-06 17:36:48 -0400

    fix mysql templates

--- mysql/dayschedule.sql
+++ mysql/dayschedule.sql
@@ -1,16 +1,20 @@
-CREATE TABLE IF NOT EXISTS `dayschedule_custom_summaries` (
-  `daytype` varchar(32) NOT NULL,
-  `daydesc` varchar(128) NOT NULL
-)
-CREATE TABLE IF NOT EXISTS `dayschedule_pretty_summaries` (
-  `daytype` varchar(32) NOT NULL,
-  `daydesc` varchar(128) NOT NULL
-)
-CREATE TABLE IF NOT EXISTS `dayschedule_custom_schedules` (
-  `daytype` varchar(32) NOT NULL,
-  `json` varchar(2048) NOT NULL
-)
-CREATE TABLE IF NOT EXISTS `dayschedule_override_schedules` (
-  `dayname` varchar(8) NOT NULL,
-  `daytype` varchar(32) NOT NULL
-)
+DROP TABLE IF EXISTS dayschedule_custom_summaries;
+CREATE TABLE dayschedule_custom_summaries (
+  daytype VARCHAR(32) NOT NULL,
+  daydesc VARCHAR(128) NOT NULL
+);
+DROP TABLE IF EXISTS dayschedule_pretty_summaries;
+CREATE TABLE dayschedule_pretty_summaries (
+  daytype VARCHAR(32) NOT NULL,
+  daydesc VARCHAR(128) NOT NULL
+);
+DROP TABLE IF EXISTS dayschedule_custom_schedules;
+CREATE TABLE dayschedule_custom_schedules (
+  daytype VARCHAR(32) NOT NULL,
+  json VARCHAR(2048) NOT NULL
+);
+DROP TABLE IF EXISTS dayschedule_override_schedules;
+CREATE TABLE dayschedule_override_schedules (
+  dayname VARCHAR(8) NOT NULL,
+  daytype VARCHAR(32) NOT NULL
+);
--- mysql/relationships.sql
+++ mysql/relationships.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS realationships;
+DROP TABLE IF EXISTS relationships;
 CREATE TABLE relationships (
 	uid MEDIUMINT(9) UNSIGNED NOT NULL,
 	device TEXT
